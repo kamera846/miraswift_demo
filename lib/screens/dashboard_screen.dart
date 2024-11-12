40f3b26b3cde302e387663c5eb19d87a30667349
Mochammad Rafli Ramadani <mochammadrafliramadani@gmail.com>
2024-11-12 15:10:30 +0700

    setup realtime database

--- lib/screens/dashboard_screen.dart
+++ lib/screens/dashboard_screen.dart
@@ -1,10 +1,40 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
 import 'package:miraswift_demo/screens/batch_screen.dart';
 import 'package:miraswift_demo/screens/equipment_screen.dart';
 import 'package:miraswift_demo/screens/product_screen.dart';
 
-class DashboardScreen extends StatelessWidget {
+FirebaseDatabase database = FirebaseDatabase.instance;
+
+class DashboardScreen extends StatefulWidget {
   const DashboardScreen({super.key});
+
+  @override
+  State<DashboardScreen> createState() => _DashboardScreenState();
+}
+
+class _DashboardScreenState extends State<DashboardScreen> {
+  late String? equipmentStatus;
+  @override
+  void initState() {
+    getFirebaseData();
+    super.initState();
+  }
+
+  void getFirebaseData() {
+    setState(() {
+      equipmentStatus = null;
+    });
+    DatabaseReference starCountRef =
+        FirebaseDatabase.instance.ref('Bizlink/equipment');
+    starCountRef.onValue.listen((DatabaseEvent event) {
+      final data = event.snapshot.value;
+      // print('Equipment Status: $data');
+      setState(() {
+        equipmentStatus = '$data';
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +50,7 @@ class DashboardScreen extends StatelessWidget {
           mainAxisSize: MainAxisSize.min,
           children: [
             const Text('Dashboard is coming soon..'),
+            Text('Equipment Status: ${equipmentStatus ?? 'waiting...'}'),
             const SizedBox(height: 12),
             ElevatedButton(
               style: ElevatedButton.styleFrom(
