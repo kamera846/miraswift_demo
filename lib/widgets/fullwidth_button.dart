08d856e8a9ea73b8c944bbb97db9fc99bc8dac2a
Mochammad Rafli Ramadani <mochammadrafliramadani@gmail.com>
2025-01-16 18:22:19 +0700

    Adding fullwidth button widget

---
+++ lib/widgets/fullwidth_button.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class FullwidthButton extends StatefulWidget {
+  const FullwidthButton(
+      {super.key, required this.onPressed, required this.child});
+
+  final VoidCallback onPressed;
+  final Widget child;
+
+  @override
+  State<FullwidthButton> createState() => _FullwidthButtonState();
+}
+
+class _FullwidthButtonState extends State<FullwidthButton> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(
+          8,
+        ),
+        boxShadow: const [
+          BoxShadow(
+            color: Colors.black12,
+            blurRadius: 1,
+            offset: Offset(0, 1),
+          ),
+        ],
+      ),
+      child: ElevatedButton(
+        onPressed: widget.onPressed,
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all(Colors.blue),
+          padding: WidgetStateProperty.all(
+            const EdgeInsets.symmetric(vertical: 12.0),
+          ),
+          shape: WidgetStateProperty.all(
+            RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+          ),
+        ),
+        child: widget.child,
+      ),
+    );
+  }
+}
