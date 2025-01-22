99515d4033afdaaff2e54ecf28be8c2bf518341e
Mochammad Rafli Ramadani <mochammadrafliramadani@gmail.com>
2025-01-22 14:38:30 +0700

    Re design dashboard v2

--- lib/screens/dashboardv2_screen.dart
+++ lib/screens/dashboardv2_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:miraswift_demo/screens/batch_screen.dart';
+import 'package:miraswift_demo/screens/equipment_screen.dart';
+import 'package:miraswift_demo/screens/notifications_screen.dart';
+import 'package:miraswift_demo/screens/product_screen.dart';
+import 'package:miraswift_demo/screens/spk_screen.dart';
 
 class DashboardV2Screen extends StatefulWidget {
   const DashboardV2Screen({super.key});
@@ -45,33 +50,35 @@ class _DashboarV2dScreenState extends State<DashboardV2Screen> {
                       mainAxisSize: MainAxisSize.max,
                       children: [
                         Expanded(
-                            flex: 2,
-                            child: Container(
-                                margin: const EdgeInsets.all(8),
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  color: Colors.white.withOpacity(0.8),
-                                  borderRadius: BorderRadius.circular(24),
-                                  border:
-                                      Border.all(color: Colors.white, width: 2),
-                                ),
-                                child: const Center(
-                                  child: Text('Batchs'),
-                                ))),
+                          flex: 2,
+                          child: MenuItems(
+                            icon: Icons.playlist_add_circle_rounded,
+                            surfaceColor: Colors.blue,
+                            title: 'Recipes',
+                            description: '230 items',
+                            onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (ctx) => const ProductScreen(),
+                              ),
+                            ),
+                          ),
+                        ),
                         Expanded(
-                            flex: 3,
-                            child: Container(
-                                margin: const EdgeInsets.all(8),
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  color: Colors.white.withOpacity(0.8),
-                                  borderRadius: BorderRadius.circular(24),
-                                  border:
-                                      Border.all(color: Colors.white, width: 2),
-                                ),
-                                child: const Center(
-                                  child: Text('SPK'),
-                                ))),
+                          flex: 3,
+                          child: MenuItems(
+                            icon: Icons.playlist_add_check_circle,
+                            surfaceColor: Colors.red,
+                            title: 'Settings SPK',
+                            description: '230 items',
+                            onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (ctx) => const SpkScreen(),
+                              ),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -80,33 +87,35 @@ class _DashboarV2dScreenState extends State<DashboardV2Screen> {
                       mainAxisSize: MainAxisSize.max,
                       children: [
                         Expanded(
-                            flex: 3,
-                            child: Container(
-                                margin: const EdgeInsets.all(8),
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  color: Colors.white.withOpacity(0.8),
-                                  borderRadius: BorderRadius.circular(24),
-                                  border:
-                                      Border.all(color: Colors.white, width: 2),
-                                ),
-                                child: const Center(
-                                  child: Text('Recipes'),
-                                ))),
+                          flex: 3,
+                          child: MenuItems(
+                            icon: Icons.lightbulb_circle,
+                            surfaceColor: Colors.green,
+                            title: 'Batchs',
+                            description: '230 items',
+                            onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (ctx) => const BatchScreen(),
+                              ),
+                            ),
+                          ),
+                        ),
                         Expanded(
-                            flex: 2,
-                            child: Container(
-                                margin: const EdgeInsets.all(8),
-                                width: double.infinity,
-                                decoration: BoxDecoration(
-                                  color: Colors.white.withOpacity(0.8),
-                                  borderRadius: BorderRadius.circular(24),
-                                  border:
-                                      Border.all(color: Colors.white, width: 2),
-                                ),
-                                child: const Center(
-                                  child: Text('Notifications'),
-                                ))),
+                          flex: 2,
+                          child: MenuItems(
+                            icon: Icons.circle_notifications,
+                            surfaceColor: Colors.yellow,
+                            title: 'Notifications',
+                            description: '230 items',
+                            onTap: () => Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (ctx) => const NotificationsScreen(),
+                              ),
+                            ),
+                          ),
+                        ),
                       ],
                     ),
                   ),
@@ -116,17 +125,21 @@ class _DashboarV2dScreenState extends State<DashboardV2Screen> {
           ),
           Expanded(
             flex: 1,
-            child: Container(
-                margin: const EdgeInsets.symmetric(horizontal: 16, vertical: 8),
-                width: double.infinity,
-                decoration: BoxDecoration(
-                  color: Colors.white.withOpacity(0.8),
-                  borderRadius: BorderRadius.circular(24),
-                  border: Border.all(color: Colors.white, width: 2),
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 8),
+              child: MenuItems(
+                icon: Icons.build_circle,
+                surfaceColor: Colors.purple,
+                title: 'Monitoring Equipments',
+                description: 'Looking your equipments in realtime',
+                onTap: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (ctx) => const EquipmentScreen(),
+                  ),
                 ),
-                child: const Center(
-                  child: Text('Monitoring Equipments'),
-                )),
+              ),
+            ),
           ),
           Container(
               padding: const EdgeInsets.all(16),
@@ -146,6 +159,95 @@ class _DashboarV2dScreenState extends State<DashboardV2Screen> {
                 child: Text('© 2024 Miraswift Auto Solusi'),
               )),
         ],
+      ),
+    );
+  }
+}
+
+class MenuItems extends StatelessWidget {
+  const MenuItems({
+    super.key,
+    required this.onTap,
+    required this.icon,
+    required this.title,
+    required this.description,
+    this.surfaceColor = Colors.blue,
+    this.margin = const EdgeInsets.all(8),
+    this.padding = const EdgeInsets.all(12),
+  });
+
+  final IconData icon;
+  final String title;
+  final String description;
+  final Color? surfaceColor;
+  final EdgeInsets? margin;
+  final EdgeInsets? padding;
+  final Function() onTap;
+
+  @override
+  Widget build(BuildContext context) {
+    return InkWell(
+      onTap: onTap,
+      splashColor: surfaceColor!.withOpacity(0.3),
+      highlightColor: Colors.transparent,
+      borderRadius: BorderRadius.circular(24),
+      child: Container(
+        margin: margin,
+        padding: margin,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: Colors.white.withOpacity(0.8),
+          borderRadius: BorderRadius.circular(24),
+          border: Border.all(color: Colors.white, width: 2),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  icon,
+                  color: surfaceColor,
+                ),
+                const SizedBox(width: 12),
+                Text(
+                  title,
+                  style: Theme.of(context).textTheme.titleMedium,
+                ),
+              ],
+            ),
+            const SizedBox(height: 6),
+            Expanded(
+              child: Text(
+                description,
+                style: Theme.of(context).textTheme.bodySmall,
+                maxLines: 2,
+                softWrap: true,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            const SizedBox(height: 12),
+            const Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(
+                  'Open',
+                  style: TextStyle(color: Colors.blue),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  width: 8,
+                ),
+                Icon(
+                  Icons.arrow_circle_right_sharp,
+                  size: 18,
+                  color: Colors.blue,
+                ),
+              ],
+            ),
+            const SizedBox(height: 12),
+          ],
+        ),
       ),
     );
   }
