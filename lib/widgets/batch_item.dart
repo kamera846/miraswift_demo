782019e6b87f0007154466c81f19e07a42bdef0e
Mochammad Rafli Ramadani <mochammadrafliramadani@gmail.com>
2025-01-24 16:30:17 +0700

    adding difference actual

--- lib/widgets/batch_item.dart
+++ lib/widgets/batch_item.dart
@@ -29,6 +29,75 @@ class BatchItem extends StatelessWidget {
       statusColorBorderOff = Colors.yellow.shade900.withAlpha(150);
     }
 
+    double different = 0;
+    Widget actualIcon = const SizedBox.shrink();
+    Widget actualDifferent = const SizedBox.shrink();
+
+    if (scales != null) {
+      if (scales!.formula != null) {
+        var targetFormula = double.parse(scales!.formula!.targetFormula);
+        var fineFormula = double.parse(scales!.formula!.fineFormula);
+        var totalFormula = targetFormula + fineFormula;
+        var actualTimbang = double.parse(scales!.actualTimbang);
+        // isOverLimit = totalFormula < actualTimbang;
+        // if (isOverLimit) {
+        //   different = actualTimbang - totalFormula;
+        // } else {
+        //   different = totalFormula - actualTimbang;
+        // }
+        if (actualTimbang < targetFormula) {
+          // kurang dari target
+          different = targetFormula - actualTimbang;
+          actualIcon = const Icon(
+            Icons.keyboard_double_arrow_down,
+            color: Colors.orange,
+            size: 12,
+          );
+          actualDifferent = Text(
+            different.toStringAsFixed(1),
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall!
+                .copyWith(color: Colors.orange),
+          );
+        } else if (actualTimbang == targetFormula) {
+          // pas
+          different = 0;
+        } else if (actualTimbang > targetFormula &&
+            actualTimbang <= totalFormula) {
+          // masuk range
+          different = actualTimbang - targetFormula;
+          actualIcon = const Icon(
+            Icons.keyboard_double_arrow_up,
+            color: Colors.green,
+            size: 12,
+          );
+          actualDifferent = Text(
+            different.toStringAsFixed(1),
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall!
+                .copyWith(color: Colors.green),
+          );
+        } else {
+          // melebihi range
+          different = actualTimbang - targetFormula;
+          actualIcon = const Icon(
+            Icons.keyboard_double_arrow_up,
+            color: Colors.red,
+            size: 12,
+          );
+          actualDifferent = Text(
+            different.toStringAsFixed(1),
+            style: Theme.of(context)
+                .textTheme
+                .bodySmall!
+                .copyWith(color: Colors.red),
+          );
+        }
+      }
+    }
+
     return Column(
       children: [
         Row(
@@ -119,15 +188,19 @@ class BatchItem extends StatelessWidget {
                     ),
                   ] else if (equipment == null && scales != null) ...[
                     const SizedBox(height: 8),
-                    Text(
-                      scales!.formula != null
-                          ? 'Target formula ${scales!.formula!.targetFormula} kg (Fine ${scales!.formula!.fineFormula} kg)'
-                          : '-',
-                      style: Theme.of(context)
-                          .textTheme
-                          .bodySmall!
-                          .copyWith(color: Colors.grey.shade600),
-                    ),
+                    Row(
+                      children: [
+                        Text(
+                          scales!.formula != null
+                              ? 'Target formula ${scales!.formula!.targetFormula} kg (Fine ${scales!.formula!.fineFormula} kg)'
+                              : '-',
+                          style: Theme.of(context)
+                              .textTheme
+                              .bodySmall!
+                              .copyWith(color: Colors.grey.shade600),
+                        ),
+                      ],
+                    )
                   ]
                 ],
               ),
@@ -145,8 +218,14 @@ class BatchItem extends StatelessWidget {
                     color: statusColorBorderOff,
                   )
                 else if (equipment == null)
-                  Text('${scales!.actualTimbang} KG',
-                      style: Theme.of(context).textTheme.bodySmall),
+                  Row(
+                    children: [
+                      Text('${scales!.actualTimbang} KG',
+                          style: Theme.of(context).textTheme.bodySmall),
+                      actualIcon,
+                      actualDifferent,
+                    ],
+                  ),
                 Text(
                   equipment != null
                       ? equipment!.desc.toUpperCase()
