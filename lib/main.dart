f18baa738fdde75a2398f3ff8713a065fa5a343e
Mochammad Rafli Ramadani <mochammadrafliramadani@gmail.com>
2025-08-09 11:17:44 +0700

    set fixed rotation and text scale

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:intl/date_symbol_data_local.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:miraswiftdemo/screens/dashboardv3_screen.dart';
@@ -8,6 +9,7 @@ void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await initializeDateFormatting("id_ID", null);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
   runApp(const MainApp());
 }
 
@@ -16,26 +18,35 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Miraswift Demo',
-      theme: ThemeData().copyWith(
-        scaffoldBackgroundColor: Colors.white,
-        primaryColor: Colors.blue,
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
-        appBarTheme: const AppBarTheme(
-          color: Colors.white,
-          elevation: 0,
-          scrolledUnderElevation: 1,
-          shadowColor: Colors.white54,
-        ),
-        bottomSheetTheme: const BottomSheetThemeData(
-          backgroundColor: Colors.white,
-        ),
-        popupMenuTheme: const PopupMenuThemeData(color: Colors.white),
-        dialogTheme: const DialogThemeData(backgroundColor: Colors.white),
-        splashColor: Colors.blue.withValues(alpha: 0.1),
-      ),
-      home: const DashboardV3Screen(),
+    return Builder(
+      builder: (context) {
+        return MediaQuery(
+          data: MediaQuery.of(
+            context,
+          ).copyWith(textScaler: TextScaler.linear(1.0)),
+          child: MaterialApp(
+            title: 'Miraswift Demo',
+            theme: ThemeData().copyWith(
+              scaffoldBackgroundColor: Colors.white,
+              primaryColor: Colors.blue,
+              colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+              appBarTheme: const AppBarTheme(
+                color: Colors.white,
+                elevation: 0,
+                scrolledUnderElevation: 1,
+                shadowColor: Colors.white54,
+              ),
+              bottomSheetTheme: const BottomSheetThemeData(
+                backgroundColor: Colors.white,
+              ),
+              popupMenuTheme: const PopupMenuThemeData(color: Colors.white),
+              dialogTheme: const DialogThemeData(backgroundColor: Colors.white),
+              splashColor: Colors.blue.withValues(alpha: 0.1),
+            ),
+            home: const DashboardV3Screen(),
+          ),
+        );
+      },
     );
   }
 }
